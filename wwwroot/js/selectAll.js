7a37351adbda9a9aa2fa5c09f60ee74fd0eacec4
andreyk2002 <56173069+andreyk2002@users.noreply.github.com>
2020-12-16 14:54:42 +0300

    Add files via upload

---
+++ wwwroot/js/selectAll.js
@@ -0,0 +1,12 @@
+﻿let dataTable = document.getElementById('data-table');
+let checkItAll = dataTable.querySelector('input[name="select_all"]');
+let inputs = dataTable.querySelectorAll('tbody>tr>td>input');
+let deleteButton = document.getElementById('Delete');
+let blockButton = document.getElementById('Block');
+let unblockButton = document.getElementById('Unblock');
+
+checkItAll.addEventListener('change', function () {
+     inputs.forEach(function (input) {
+            input.checked = checkItAll.checked;
+     });
+});
